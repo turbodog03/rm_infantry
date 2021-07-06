2b17c4fdd78cd650016c01be4d5275632d6feec7
turbodog03 <63595854+turbodog03@users.noreply.github.com>
2021-07-06 20:48:47 +0800

    clean git cache, update gitignore

--- Project/rm_infantry/crc16.d
+++
@@ -1,2 +0,0 @@
-.\rm_infantry\crc16.o: crc16.c
-.\rm_infantry\crc16.o: D:\keil v5 core\ARM\ARMCC\Bin\..\include\stdint.h
--- Project/rm_infantry/crc8_crc16.d
+++
@@ -1,5 +0,0 @@
-.\rm_infantry\crc8_crc16.o: ..\User\referee\CRC8_CRC16.c
-.\rm_infantry\crc8_crc16.o: ..\User\referee\crc8_crc16.h
-.\rm_infantry\crc8_crc16.o: D:\keil v5 core\ARM\ARMCC\Bin\..\include\stdint.h
-.\rm_infantry\crc8_crc16.o: ..\Inc\main.h
-.\rm_infantry\crc8_crc16.o: D:\keil v5 core\ARM\ARMCC\Bin\..\include\stdio.h
